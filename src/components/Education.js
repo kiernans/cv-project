9aa89a9aabcd6bb1cff10bdd0c4ec8438ae43145
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-07-08 11:52:03 -0700

    Create boilerplate for components

---
+++ src/components/Education.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Education extends Component {
+    constructor() {
+        super();
+    };
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    };
+};
+
+export default Education;
---
+++ src/components/Information.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Information extends Component {
+    constructor() {
+        super();
+    };
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    };
+};
+
+export default Information;
